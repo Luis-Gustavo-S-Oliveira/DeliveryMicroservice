44edaa7769a969e0fcbedc3cb34ce9abcdee6262
luis <luisgu6666@gmail.com>
2026-01-30 18:03:58 -0300

    Hotflix incompleto

--- Microservice/Delivery-Tracking/src/main/java/com/microservice/deliveryalga/delivery/tracking/domain/model/Delivery.java
+++ Microservice/Delivery-Tracking/src/main/java/com/microservice/deliveryalga/delivery/tracking/domain/model/Delivery.java
@@ -1,5 +1,6 @@
 package com.microservice.deliveryalga.delivery.tracking.domain.model;
 import com.microservice.deliveryalga.delivery.tracking.domain.exception.DomainException;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.Duration;
@@ -10,12 +11,14 @@ import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.UUID;
 
+@Entity
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Setter(AccessLevel.PRIVATE)
 @Getter
 public class Delivery {
 
+    @Id
     @EqualsAndHashCode.Include
     private UUID id;
     private UUID courierId;
@@ -33,9 +36,28 @@ public class Delivery {
 
     private Integer totalItems;
 
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "zipCode",column = @Column(name = "sender_zip_code")),
+            @AttributeOverride(name = "street",column = @Column(name = "sender_street")),
+            @AttributeOverride(name = "number",column = @Column(name = "sender_number")),
+            @AttributeOverride(name = "complement",column = @Column(name = "sender_complement")),
+            @AttributeOverride(name = "name",column = @Column(name = "sender_name")),
+            @AttributeOverride(name = "phone",column = @Column(name = "sender_phone"))
+    })
     private ContactPoint sender;
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "zipCode",column = @Column(name = "recipient_zip_code")),
+            @AttributeOverride(name = "street",column = @Column(name = "recipient_street")),
+            @AttributeOverride(name = "number",column = @Column(name = "recipient_number")),
+            @AttributeOverride(name = "complement",column = @Column(name = "recipient_complement")),
+            @AttributeOverride(name = "name",column = @Column(name = "recipient_name")),
+            @AttributeOverride(name = "phone",column = @Column(name = "recipient_phone"))
+    })
     private ContactPoint recipient;
 
+   @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true,mappedBy = "delivery")
     private List<Item> items = new ArrayList<>();
 
     public static Delivery draft(){
@@ -50,7 +72,7 @@ public class Delivery {
     }
 
     public UUID addItem(String name, int quantity){
-        Item item = Item.brandNew(name, quantity);
+        Item item = Item.brandNew(name, quantity,this);
         items.add(item);
         calculateTotalItems();
         return item.getId();
@@ -98,7 +120,7 @@ public class Delivery {
     }
 
     public void markAsDelivered(){
-        this.changeStatus(DeliveryStatus.DELIVERY);
+        this.changeStatus(DeliveryStatus.DELIVERED);
         this.setFulfilledAt(OffsetDateTime.now());
     }
 
