d09d7b54df5ef134f2a325c445b6158ec3e10073
Luis-Gustavo-S-Oliveira <luisgulg2000@gmail.com>
2026-03-02 17:48:27 -0300

    add persistence courier

--- Microservice/Courier-Management/src/main/java/com/microservice/deliveryalga/courier/management/domain/model/AssignedDelivery.java
+++ Microservice/Courier-Management/src/main/java/com/microservice/deliveryalga/courier/management/domain/model/AssignedDelivery.java
@@ -1,25 +1,36 @@
 package com.microservice.deliveryalga.courier.management.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.*;
 
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+@Entity
 @Getter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Setter(AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PACKAGE )
 public class AssignedDelivery {
 
+    @Id
     @EqualsAndHashCode.Include
     private UUID id;
 
     private OffsetDateTime assignedAt;
 
-    static  AssignedDelivery pending(UUID deliveryId){
+    @ManyToOne(optional = false)
+    @Getter(AccessLevel.PRIVATE)
+    private Courier courier;
+
+    static  AssignedDelivery pending(UUID deliveryId, Courier courier) {
         AssignedDelivery delivery = new AssignedDelivery();
         delivery.setId(deliveryId);
         delivery.setAssignedAt(OffsetDateTime.now());
+        delivery.setCourier(courier);
         return delivery;
     };
 
